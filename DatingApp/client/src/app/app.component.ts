b1fad76b5692e3c5487ec7c8dce7867116e0afe0
EnesHarunANASOY <enesanasoy@gmail.com>
2024-01-10 00:05:31 +0300

    End of section 4

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Dating App';
+  title = 'Dating App & ENES';
   users: any;
 
   constructor(private http: HttpClient){}
